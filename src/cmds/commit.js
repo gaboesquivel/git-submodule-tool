d595e26f7b4db44e757aec7c5b4873e84ff9db24
Ryan Wilson <rywils21@gmail.com>
2019-03-15 11:08:58 -0700

    Added error message if -m not specified with commit

--- src/cmds/commit.js
+++ src/cmds/commit.js
@@ -1,17 +1,18 @@
 const shell = require("shelljs");
+const chalk = require("chalk");
 
 module.exports = (args) => {
-    console.log('called gst ', args);
-
     let cmd = 'commit';
 
     if (args.m) {
         cmd = `commit -m "${args.m}"`;
+
+        shell.exec(`git submodule foreach --recursive 'git add . || :'`);
+        shell.exec(`git submodule foreach --recursive 'git ${cmd} || :'`);
+        shell.exec(`git add .`)
+        shell.exec(`git ${cmd}`);
+    } else {
+        console.error(chalk.red(`ERROR: Console editors not supported. Must use 'gst commit -m "{message}"`));
     }
 
-    `'npm install || :'`
-    shell.exec(`git submodule foreach --recursive 'git add . || :'`);
-    shell.exec(`git submodule foreach --recursive 'git ${cmd} || :'`);
-    shell.exec(`git add .`)
-    shell.exec(`git ${cmd}`);
 }
